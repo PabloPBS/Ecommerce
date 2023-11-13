600a10749ec6a97d628d5300abb74559de19f988
Pablo Barbosa Silva <pablopbs940@gmail.com>
2023-11-12 21:01:04 -0300

    scroll desativado quando menu está aberto

--- scripts/menuMobile.js
+++ scripts/menuMobile.js
@@ -2,6 +2,7 @@ const menuMobile = document.querySelector('#menu')
 const abrirMenu = document.querySelector('#abrir-menu')
 const fecharMenu = document.querySelector('#fechar-menu')
 const botoesMenu = document.getElementsByClassName('botao-menu')
+body = document.getElementsByTagName('body')[0]
 
 for (let i = 0; i < botoesMenu.length; i++) {
     botoesMenu[i].addEventListener('click', hide)
@@ -10,8 +11,10 @@ for (let i = 0; i < botoesMenu.length; i++) {
 function show() {
     menuMobile.style.display= 'flex'
     menuMobile.style.top = 0
+    body.style.overflow = 'hidden'
 }
 
 function hide() {
     menuMobile.style.top = '-120%'
+    body.style.overflow = 'auto'
 }
